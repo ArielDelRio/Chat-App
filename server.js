83412759a26cd8966ab795f3ea10a4e3466fad59
ArieldRio <arieldelrioviamonte@gmail.com>
2021-07-01 16:53:17 -0400

    Configurations for dev, comunicate front and backend in development

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get("/test", test);
 app.use(express.static("build"));
 
 // Live Server
-const PORT = 3000;
+const PORT = 3001;
 app.listen(process.env.PORT || PORT, (err) => {
   if (err) {
     console.error(err);
--- src/config.js
+++ src/config.js
@@ -3,9 +3,11 @@ export const TOKEN_SIGNED_IN = "CHAT-APP-GOOGLE_PROVIDER_SIGNEDIN";
 export const GOOGLE_CLIENT_ID =
   "325689536941-3epu1qj2a2tkhb73v1g5bgh58dhtjckj.apps.googleusercontent.com";
 
+const dev = true;
+
 export const PUSHER_CONFIG = {
   public_key: "664c0cde38c2cec0cdc9",
   cluster: "us2",
-  authEndpoint: `./pusher/auth`,
+  authEndpoint: dev ? "http://localhost:3001/pusher/auth" : `./pusher/auth`,
   main_channel: "presence-main",
 };
