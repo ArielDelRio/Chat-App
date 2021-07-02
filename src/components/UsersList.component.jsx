950b8208d0512d98fa1356ad22ff3a9929740ff4
ArieldRio <arieldelrioviamonte@gmail.com>
2021-07-02 19:24:53 -0400

    Use Viewed Status in User List Component for display badges about unread messages

--- src/components/UsersList.component.jsx
+++ src/components/UsersList.component.jsx
@@ -7,46 +7,61 @@ import ListItemText from "@material-ui/core/ListItemText";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
 import { Badge } from "@material-ui/core";
-import ChatNewMessagesIcon from '@material-ui/icons/Chat';
-import { getPrivateChannelName } from '../utils';
+import ChatNewMessagesIcon from "@material-ui/icons/Chat";
+import { getPrivateChannelName } from "../utils";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     margin: "auto",
     maxWidth: "64ch",
     backgroundColor: theme.palette.background.paper,
-    paddingTop: 0
+    paddingTop: 0,
   },
   inline: {
     display: "inline",
   },
 }));
 
-const UsersList = ({ listItems, user, indexChannelSelected, handleItemClick }) => {
-  
+const UsersList = ({
+  listItems,
+  user,
+  indexChannelSelected,
+  handleItemClick,
+}) => {
   const classes = useStyles();
 
   const MainChannel = () => {
     const main_channel = listItems[0];
-    const newMessagesCount = main_channel.messages.filter((message) => !message.viewed && message.senderId !== user.id).length;
-    const lastMessage = main_channel.messages.length > 0 ? main_channel.messages[main_channel.messages.length - 1].text : '';
-    const isSelected = main_channel.channel.name === listItems[indexChannelSelected].channel.name;
+    const newMessagesCount = main_channel.messages.filter(
+      (message) => message.status !== "VIEWED" && message.senderId !== user.id
+    ).length;
+    const lastMessage =
+      main_channel.messages.length > 0
+        ? main_channel.messages[main_channel.messages.length - 1].text
+        : "";
+    const isSelected =
+      main_channel.channel.name ===
+      listItems[indexChannelSelected].channel.name;
 
     return (
       <div onClick={handleItemClick}>
         <ListItem alignItems="flex-start" button selected={isSelected}>
           <ListItemAvatar>
-            <Avatar alt={main_channel.name} src='logo2.png' variant='rounded'/>
+            <Avatar alt={main_channel.name} src="logo2.png" variant="rounded" />
           </ListItemAvatar>
           <ListItemText
             primary="Chat-App"
-            secondary={lastMessage.length > 15 ? lastMessage.slice(0, 15) + '...' : lastMessage}
+            secondary={
+              lastMessage.length > 15
+                ? lastMessage.slice(0, 15) + "..."
+                : lastMessage
+            }
           />
-          { newMessagesCount > 0 && 
-          <Badge badgeContent={newMessagesCount} color="secondary">
-            <ChatNewMessagesIcon />
-          </Badge>
-          }
+          {newMessagesCount > 0 && (
+            <Badge badgeContent={newMessagesCount} color="secondary">
+              <ChatNewMessagesIcon />
+            </Badge>
+          )}
         </ListItem>
         <Divider component="li" />
       </div>
@@ -54,17 +69,30 @@ const UsersList = ({ listItems, user, indexChannelSelected, handleItemClick }) =
   };
 
   const mainChannelMembers = listItems[0].members;
-  
+
   return (
     <List className={classes.root}>
       <MainChannel />
       {Object.values(mainChannelMembers).map((member) => {
-        
-        const channelItem = listItems.find((item) => item.channel.name === getPrivateChannelName(user.id, member.user_id));
-        const newMessagesCount = channelItem && channelItem.messages.filter((message) => !message.viewed && message.senderId !== user.id).length;
-        const lastMessage = channelItem && channelItem.messages.length > 0 ? channelItem.messages[channelItem.messages.length - 1].text : '';
-        const isSelected = channelItem && channelItem.channel.name === listItems[indexChannelSelected].channel.name;
-        
+        const channelItem = listItems.find(
+          (item) =>
+            item.channel.name === getPrivateChannelName(user.id, member.user_id)
+        );
+        const newMessagesCount =
+          channelItem &&
+          channelItem.messages.filter(
+            (message) =>
+              message.status !== "VIEWED" && message.senderId !== user.id
+          ).length;
+        const lastMessage =
+          channelItem && channelItem.messages.length > 0
+            ? channelItem.messages[channelItem.messages.length - 1].text
+            : "";
+        const isSelected =
+          channelItem &&
+          channelItem.channel.name ===
+            listItems[indexChannelSelected].channel.name;
+
         return (
           <div key={member.user_id} onClick={() => handleItemClick(member)}>
             <ListItem alignItems="flex-start" button selected={isSelected}>
@@ -73,13 +101,17 @@ const UsersList = ({ listItems, user, indexChannelSelected, handleItemClick }) =
               </ListItemAvatar>
               <ListItemText
                 primary={member.name}
-                secondary={lastMessage.length > 15 ? lastMessage.slice(0, 15) + '...' : lastMessage}
+                secondary={
+                  lastMessage.length > 15
+                    ? lastMessage.slice(0, 15) + "..."
+                    : lastMessage
+                }
               />
-              {newMessagesCount > 0 &&
+              {newMessagesCount > 0 && (
                 <Badge badgeContent={newMessagesCount} color="secondary">
                   <ChatNewMessagesIcon />
                 </Badge>
-              }
+              )}
             </ListItem>
             <Divider component="li" />
           </div>
