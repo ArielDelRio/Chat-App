f61c11aa1db4bf7d8890a38f370dc2994af20431
ArieldRio <arieldelrioviamonte@gmail.com>
2021-07-02 19:20:31 -0400

    Set different status to InfoMessage Component

--- src/components/InfoMessage.component.jsx
+++ src/components/InfoMessage.component.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import DoneIcon from "@material-ui/icons/Done";
+import DoneAllIcon from "@material-ui/icons/DoneAll";
 
-const InfoMessage = () => {
-  return <AccessTimeIcon color="disabled" style={{ fontSize: ".7em" }} />;
+const InfoMessage = ({ status }) => {
+  if (status === "SENDING")
+    return <AccessTimeIcon color="disabled" style={{ fontSize: ".7em" }} />;
+  else if (status === "DELIVERED")
+    return <DoneAllIcon color="disabled" style={{ fontSize: ".7em" }} />;
+  else if (status === "VIEWED")
+    return <DoneAllIcon color="primary" style={{ fontSize: ".7em" }} />;
 };
 
 export default InfoMessage;
